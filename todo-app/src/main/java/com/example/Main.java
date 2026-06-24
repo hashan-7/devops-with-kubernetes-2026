eb8de2c6e6dce24395294041d9d5a5ff5e44e6f4
hashan-7 <chamirahashan0707@gmail.com>
2026-06-24 22:08:05 +0530

    Add exercise 1.5 env variables and port-forward testing

--- todo-app/src/main/java/com/example/Main.java
+++ todo-app/src/main/java/com/example/Main.java
@@ -7,16 +7,20 @@ import java.net.InetSocketAddress;
 public class Main {
     public static void main(String[] args) throws IOException {
         String portEnv = System.getenv("PORT");
+
         int port = (portEnv != null) ? Integer.parseInt(portEnv) : 8080;
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
+
         server.createContext("/", exchange -> {
-            String response = "Todo App Backend";
+            String response = "<html><body><h1>Todo App Backend</h1><p>Hello Worlds</p></body></html>";
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
             exchange.sendResponseHeaders(200, response.length());
             exchange.getResponseBody().write(response.getBytes());
             exchange.getResponseBody().close();
         });
+
 
         server.start();
         System.out.println("Server started in port " + port);
