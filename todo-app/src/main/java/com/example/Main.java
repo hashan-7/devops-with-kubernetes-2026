f98f2aee5310b6d5e0ae5925b60be890760ec2a7
hashan-7 <chamirahashan0707@gmail.com>
2026-06-24 16:34:03 +0530

    Add exercise 1.2 todo-app web server

---
+++ todo-app/src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String portEnv = System.getenv("PORT");
+        int port = (portEnv != null) ? Integer.parseInt(portEnv) : 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Todo App Backend";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.getResponseBody().close();
+        });
+
+        server.start();
+        System.out.println("Server started in port " + port);
+    }
+}
