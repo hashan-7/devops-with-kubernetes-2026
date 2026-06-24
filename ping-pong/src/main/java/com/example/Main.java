92d1d6b10a25198c98f979b0436701e1c45f544f
hashan-7 <chamirahashan0707@gmail.com>
2026-06-24 23:26:16 +0530

    Add exercise 1.9 ping-pong app and shared ingress

---
+++ ping-pong/src/main/java/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/pingpong", exchange -> {
+            String response = "pong " + counter.getAndIncrement();
+            exchange.getResponseHeaders().set("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.getResponseBody().close();
+        });
+
+        server.start();
+        System.out.println("Ping-pong server started on port " + port);
+    }
+}
